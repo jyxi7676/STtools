a75927e898bf4bbb4f407910d2306d367f8bbf9d
Jingyue Xi <jyxi@fantasia.csgstat.sph.umich.edu>
2021-07-12 14:48:55 -0400

    reorganizing STtools repo

--- sttools.py
+++ sttools.py
@@ -1,3 +1,11 @@
+def str2bool(value):
+    if value.lower() in {'false', 'f', '0', 'no', 'n'}:
+        return False
+    elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
+        return True
+    raise ValueError(f'{value} is not a valid boolean value')
+
+
 #modules
 import argparse
 import os, sys
